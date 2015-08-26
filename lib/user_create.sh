2fc95f3b2b5010d80e377c86a688667224f20471
Brad King <brad.king@utah.edu>
2015-08-26 13:08:38 -0600

    TODO mention

--- lib/user_create.sh
+++ lib/user_create.sh
@@ -104,6 +104,7 @@ fi
 --data "State=Add_Submit&add_max=10&cls_id=&imported=0&community_id=1&init_pass=${PASSWORD}&retype_pass=${PASSWORD}&name_gen_enable=1&name_gen_type=0&display_name_format=0&user_1=${USERNAME}&gname_1=${FIRSTNAME}&fname_1=${LASTNAME}&dname_1=${FIRSTNAME}+${LASTNAME}&type_1=S&email_1=${EMAIL}&user_2=&gname_2=&fname_2=&dname_2=&type_2=S&email_2=&user_3=&gname_3=&fname_3=&dname_3=&type_3=S&email_3=&user_4=&gname_4=&fname_4=&dname_4=&type_4=S&email_4=&user_5=&gname_5=&fname_5=&dname_5=&type_5=S&email_5=&user_6=&gname_6=&fname_6=&dname_6=&type_6=S&email_6=&user_7=&gname_7=&fname_7=&dname_7=&type_7=S&email_7=&user_8=&gname_8=&fname_8=&dname_8=&type_8=S&email_8=&user_9=&gname_9=&fname_9=&dname_9=&type_9=S&email_9=&user_10=&gname_10=&fname_10=&dname_10=&type_10=S&email_10=" \
 --compressed > /dev/null
 
+# This needs to be modified to fail if a user already exists
 if [ "$?" != 0 ]; then
 	log "FAILURE: Could not create Student: ${USERNAME}, ${FIRSTNAME}, ${LASTNAME}: curl exit code: $?"
 	exit 1
